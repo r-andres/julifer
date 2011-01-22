ad692460445605821e8aa1e8d1a7333c32c29f67
monica.fbarreiro <monica.fbarreiro@gmail.com>
2011-01-22 19:21:43 +0000

    Extending factura widget.

--- Julifer/lib/model/Factura.php
+++ Julifer/lib/model/Factura.php
@@ -1,29 +1,28 @@
 <?php
 class Factura {
 	var $id, $fecha, $estado, $vehiculo, $servicios, $mecanica, $totalMecanica, $descuentoMecanica,  $pintura, $totalPintura, $descuentoPintura, $franquicia, $pagado;
-	var $IVA = 18.0;
 	const TIPO_FACTURA = "FACTURA";
 	const TIPO_PRESUPUESTO = "PRESUPUESTO";
 	const ESTADO_PAGADO = "PAGADO";
 	const ESTADO_PENDIENTE = "PENDIENTE";
+	const IVA = 18.0;
 	
 	function dump ($arr) {
-		
 		$this->id = $arr['id'];
 		$this->fecha = $arr['fecha'];
 		$this->tipo = $arr['tipo'];
 		$this->vehiculo = $arr['vehiculo'];
 		$this->servicios =  array ();
 		$this->mecanica = $arr['mecanica'];
-		$this->totalMecanica = $arr['totalMecanica'];
-		$this->descuentoMecanica = $arr['descuentoMecanica'];
+		$this->totalMecanica = $arr['totalMecanica'] == ""?0:$arr['totalMecanica'];
+		$this->descuentoMecanica =$arr['descuentoMecanica'] == ""?0:$arr['descuentoMecanica']; 
 		
 		$this->pintura = $arr['pintura'];
-		$this->totalPintura = $arr['totalPintura'];
-		$this->descuentoPintura = $arr['descuentoPintura'];
+		$this->totalPintura =  $arr['totalPintura'] == ""?0:$arr['totalPintura'];
+		$this->descuentoPintura = $arr['descuentoPintura'] == ""?0:$arr['descuentoPintura'];
 		
-		$this->pagado = $arr['pagado'];
-		$this->franquicia = $arr['franquicia'];
+		$this->pagado = $arr['pagado'] == ""?0:$arr['pagado'];
+		$this->franquicia = $arr['franquicia'] == ""?0:$arr['franquicia'];
 		
 		$keys = array_keys($arr); 
 //		usort($keys, "Factura::cmp" );
@@ -49,6 +48,13 @@ class Factura {
 		}
 	}
 	
+	function searchDump ($arr) {		
+		$this->id = $arr['id'];
+		$this->fecha = $arr['fecha'];
+		$this->tipo = $arr['tipo'];
+		$this->pagado = $arr['pagado'] == ""?0:$arr['pagado'];
+		$this->franquicia = $arr['franquicia'] == ""?0:$arr['franquicia'];
+	}
 	
 	function cmp ($a, $b) {
 		$aComponents = explode("_", $a);
@@ -107,14 +113,13 @@ class Factura {
 	
 	
 	function total() {
-		$total = 0; 
-		
+		$total = 0; 		
 		
 		$total += $this->totalMateriales();
 		$total += $this->totalChapa();
 		$total += $this->totalPintura();
 
-		$total += ($total * $this->IVA) / 100;
+		$total += ($total * self::IVA) / 100;
 		
 		$total -= $this->franquicia;
 		
