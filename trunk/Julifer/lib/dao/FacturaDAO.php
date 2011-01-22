5abb1514c81ab21300a9cdef423d3b0fb8cd9d38
monica.fbarreiro@gmail.com <monica.fbarreiro@gmail.com@79dfa939-cda0-1ce1-1028-bca9c6da6be9>
2011-01-22 19:22:03 +0000

    Extending factura widget.

--- trunk/Julifer/lib/dao/FacturaDAO.php
+++ trunk/Julifer/lib/dao/FacturaDAO.php
@@ -1,113 +1,113 @@
 <?php
 
 class FacturaDAO  {
-   var $conn;
-   var $TABLE_NAME = 'facturas';
+	var $conn;
+	var $TABLE_NAME = 'facturas';
 
-   function FacturaDAO(&$conn) {
-     $this->conn = $conn;
-   }
+	function FacturaDAO(&$conn) {
+		$this->conn = $conn;
+	}
 
-   function save(&$vo) {
-     if ($vo->id == 0) {
-       $this->insert($vo);
-     } else {
-       $this->update($vo);
-     }
-   }
-
-
-   function get($id) {
-     #execute select statement
-     #create new vo and call getFromResult
-     #return vo
-
-     $list = $this->filteredList(" id = '$id' ", "");
-     return  $list[0];
-
-   }
-
-   function delete(&$vo) {
- 
-     $query = "DELETE FROM  $this->TABLE_NAME WHERE id = $vo->id "; 
-   	 $result = mysql_query($query, $this->conn) or db__showError();
-   }
-
-   function filteredList ($filter, $orderby) {
-        $list =  array () ;
-        $query = "SELECT * , DATE_FORMAT(fecha,  '%Y-%m-%d') as cfecha  FROM  $this->TABLE_NAME ";
-        if($filter != "")
-        {
-            $query .= " WHERE $filter ";
-        }
-        if($orderby != "")
-        {
-            $query .= " ORDER BY  $orderby ";
-        }
-       
-        $result = mysql_query($query,$this->conn) or db__showError();
-
-        if ($myrow = mysql_fetch_array($result)) {
-            do {
-                $element = new Factura ();
-                $this->getFromResult($element, $myrow);
-                array_push($list,$element);
-            } while ($myrow = mysql_fetch_array($result));
-        }
-        mysql_free_result($result);
-        return $list;   
-   }
+	function save(&$vo) {
+		if ($vo->id == 0) {
+			$this->insert($vo);
+		} else {
+			$this->update($vo);
+		}
+	}
 
 
-   #-- private functions
+	function get($id) {
+		#execute select statement
+		#create new vo and call getFromResult
+		#return vo
+
+		$list = $this->filteredList(" id = '$id' ", "");
+		return  $list[0];
+
+	}
+
+	function delete(&$vo) {
+
+		$query = "DELETE FROM  $this->TABLE_NAME WHERE id = $vo->id ";
+		$result = mysql_query($query, $this->conn) or db__showError();
+	}
+
+	function filteredList ($filter, $orderby) {
+		$list =  array () ;
+		$query = "SELECT * , DATE_FORMAT(fecha,  '%Y-%m-%d') as cfecha  FROM  $this->TABLE_NAME ";
+		if($filter != "")
+		{
+			$query .= " WHERE $filter ";
+		}
+		if($orderby != "")
+		{
+			$query .= " ORDER BY  $orderby ";
+		}
+			
+		$result = mysql_query($query,$this->conn) or db__showError();
+
+		if ($myrow = mysql_fetch_array($result)) {
+			do {
+				$element = new Factura ();
+				$this->getFromResult($element, $myrow);
+				array_push($list,$element);
+			} while ($myrow = mysql_fetch_array($result));
+		}
+		mysql_free_result($result);
+		return $list;
+	}
+
+
+	#-- private functions
 
 
 
-   function getFromResult(&$vo, $result) {
-   	 $vo->id = $result['id'];
-     $vo->fecha = $result['cfecha'];
-     $vo->tipo = $result['tipo'];
-     $vo->vehiculo = $result['idvehiculo'];
-     $vo->mecanica = $result['mecanica'];
+	function getFromResult(&$vo, $result) {
+		$vo->id = $result['id'];
+		$vo->fecha = $result['cfecha'];
+		$vo->tipo = $result['tipo'];
+		$vo->vehiculo = $result['idvehiculo'];
+		$vo->mecanica = $result['mecanica'];
 	 $vo->totalMecanica = $result['totalmecanica'];
 	 $vo->descuentoMecanica = $result['descuentomecanica'];
 	 $vo->pintura = $result['pintura'];
 	 $vo->totalPintura = $result['totalpintura'];
-     $vo->descuentoPintura = $result['descuentopintura'];
-     $vo->franquicia = $result['franquicia'];
-     $vo->pagado = $result['pagado'];
-     
-     $list =  array () ;
-     $query2 = "SELECT * FROM  materialFacturados WHERE idfactura = $vo->id ";
-   	 $result2 = mysql_query($query2, $this->conn) or db__showError();
+	 $vo->descuentoPintura = $result['descuentopintura'];
+	 $vo->franquicia = $result['franquicia'];
+	 $vo->pagado = $result['pagado'];
+	  
+	 $list =  array () ;
+	 $query2 = "SELECT * FROM  materialFacturados WHERE idfactura = $vo->id ";
+	 $result2 = mysql_query($query2, $this->conn) or db__showError();
 
-        if ($myrow2 = mysql_fetch_array($result2)) {
-            do {
-                $element = new MaterialFacturado ();
-                $element->cantidad = $myrow2['cantidad'];
-                $element->id = $myrow2['id'];
-                $element->material = $myrow2['material'];
-                $element->precio = $myrow2['precio'];
-                $element->descuento = $myrow2['descuento'];
-                array_push($list,$element);
-            } while ($myrow2 = mysql_fetch_array($result2));
-        }
-      mysql_free_result($result2);
-      $vo->servicios = $list;
-   }
+	 if ($myrow2 = mysql_fetch_array($result2)) {
+	 	do {
+	 		$element = new MaterialFacturado ();
+	 		$element->cantidad = $myrow2['cantidad'];
+	 		$element->id = $myrow2['id'];
+	 		$element->material = $myrow2['material'];
+	 		$element->precio = $myrow2['precio'];
+	 		$element->descuento = $myrow2['descuento'];
+	 		array_push($list,$element);
+	 	} while ($myrow2 = mysql_fetch_array($result2));
+	 }
+	 mysql_free_result($result2);
+	 $vo->servicios = $list;
+	}
 
-   function update(&$vo) {
-   	#execute update statement here
-   	$query = "UPDATE $this->TABLE_NAME SET tipo = '$vo->tipo' ,fecha = '$vo->fecha' , idvehiculo = '$vo->vehiculo',   ". 
+	function update(&$vo) {
+		#execute update statement here
+		$query = "UPDATE $this->TABLE_NAME SET tipo = '$vo->tipo' ,fecha = '$vo->fecha' , idvehiculo = '$vo->vehiculo',   ".
    			 " franquicia = '$vo->franquicia' , pagado = '$vo->pagado',  " .
    			 " mecanica = '$vo->mecanica', totalmecanica = '$vo->totalMecanica', descuentomecanica = '$vo->descuentoMecanica', ".
    			 " pintura= '$vo->pintura', totalpintura= '$vo->totalPintura', descuentopintura= '$vo->descuentoPintura' WHERE id = '$vo->id' ";
-   	$result = mysql_query($query, $this->conn) or db__showErrorCause($query);
-   	$this->insertServiciosFacturados($vo->id, $vo->servicios, true);
-   }
+		$result = mysql_query($query, $this->conn) or db__showErrorCause($query);
+		$this->insertServiciosFacturados($vo->id, $vo->servicios, true);
+	}
 
-   function insert(&$vo) {   	
-   	$query = "INSERT INTO $this->TABLE_NAME (tipo, fecha, idvehiculo , " .
+	function insert(&$vo) {
+		$query = "INSERT INTO $this->TABLE_NAME (tipo, fecha, idvehiculo , " .
    											" franquicia, pagado, " .
    											" mecanica, totalmecanica, descuentomecanica," .
    											" pintura, totalpintura, descuentopintura) " .
@@ -115,33 +115,107 @@ class FacturaDAO  {
    					 " '$vo->franquicia', '$vo->pagado',  " .
    			     	 " '$vo->mecanica' , '$vo->totalMecanica', '$vo->descuentoMecanica' , " . 
    			     	 " '$vo->pintura' , '$vo->totalPintura', '$vo->descuentoPintura') ";
-   	$result = mysql_query($query, $this->conn) or db__showError();
-    $idFactura = mysql_insert_id($this->conn);
-    $this->insertServiciosFacturados($idFactura, $vo->servicios, false);
-   
-   }
-   
-   
-   function insertServiciosFacturados ($idFactura, $servicios, $limpiar) {
-   	    
-   		if ($limpiar) {	
-   			$query = "DELETE FROM materialFacturados  " . 
+		$result = mysql_query($query, $this->conn) or db__showErrorCause($query);
+		$idFactura = mysql_insert_id($this->conn);
+		$this->insertServiciosFacturados($idFactura, $vo->servicios, false);
+			
+	}
+
+
+	function insertServiciosFacturados ($idFactura, $servicios, $limpiar) {
+
+		if ($limpiar) {
+			$query = "DELETE FROM materialFacturados  " .
    	  		         " WHERE idfactura = $idFactura  ";
-   			$result = mysql_query($query, $this->conn) or db__showError();
-   	    }
-   	    
-   		foreach ( $servicios as $servicioFacturado) {
-   			
-   			$material = $servicioFacturado->material;
-   			$cantidad = $servicioFacturado->cantidad;
-   			$precio = $servicioFacturado->precio;
-   			$descuento = $servicioFacturado->descuento;
-   			
-   	  		$query = "INSERT INTO materialFacturados (idfactura, material, cantidad, precio, descuento) " . 
+			$result = mysql_query($query, $this->conn) or db__showError();
+		}
+
+		foreach ( $servicios as $servicioFacturado) {
+
+			$material = $servicioFacturado->material;
+			$cantidad = $servicioFacturado->cantidad;
+			$precio = $servicioFacturado->precio;
+			$descuento = $servicioFacturado->descuento;
+
+			$query = "INSERT INTO materialFacturados (idfactura, material, cantidad, precio, descuento) " .
    	  		         " VALUES ( $idFactura , '$material', $cantidad, $precio, $descuento) ";
-   			$result = mysql_query($query, $this->conn) or db__showErrorCause($query);
-   		}
-   }
- }
+			$result = mysql_query($query, $this->conn) or db__showErrorCause($query);
+		}
+	}
+
+	/**
+	 * Searches a list of facturas fitting the criteria based on client an
+	 * vehicle objects as well as factura ones.
+	 *
+	 * @param $vo
+	 * @param $vehicle
+	 * @param $client
+	 */
+	function search($vo, $vehicle, $client) {
+		$where = "";
+
+		$vo_prefix = "facturas";
+		$vehicle_prefix = "vehiculos";
+		$cliente_prefix = "clientes";
+
+		// Facturas
+		foreach($vo as $key => $value) {
+			if ( $value == "" ) {
+				continue;
+			}
+
+			if (!empty($where)) {
+				$where .= " or ";
+			}
+			$where .= "$vo_prefix.$key like '%$value%'";
+		}
+
+		// Client
+		foreach($client as $key => $value) {
+			if ( $value == "" ) {
+				continue;
+			}
+
+			if (!empty($where)) {
+				$where .= " or ";
+			}
+			$where .= "$cliente_prefix.$key like '%$value%'";
+		}
+
+		// Vehicle
+		foreach($vehicle as $key => $value) {
+			if ( $value == "" ) {
+				continue;
+			}
+
+			if (!empty($where)) {
+				$where .= " or ";
+			}
+
+			$where .= "$vehicle_prefix.$key like '%$value%'";
+		}
+
+		// Defining final query
+		$list =  array () ;
+		$query = "SELECT distinct facturas.* , DATE_FORMAT(facturas.fecha,  '%Y-%m-%d') as cfecha FROM  $this->TABLE_NAME, vehiculos, clientes ";
+		if ($where != "") {
+			// Joins
+			$where .= " and vehiculos.idcliente=clientes.id && facturas.idvehiculo=vehiculos.id";
+			$query .= " WHERE $where";
+		}
+		echo $query;
+		$result = mysql_query($query,$this->conn) or db__showError();
+
+		if ($myrow = mysql_fetch_array($result)) {
+			do {
+				$element = new Factura ();
+				$this->getFromResult($element, $myrow);
+				array_push($list,$element);
+			} while ($myrow = mysql_fetch_array($result));
+		}
+		mysql_free_result($result);
+		return $list;
+	}
+}
 
 ?>
--- trunk/Julifer/lib/dao/VehiculoDAO.php
+++ trunk/Julifer/lib/dao/VehiculoDAO.php
@@ -1,125 +1,187 @@
 <?php
 
 class VehiculoDAO  {
-   var $conn;
-   var $TABLE_NAME = 'vehiculos';
+	var $conn;
+	var $TABLE_NAME = 'vehiculos';
 
-   function VehiculoDAO(&$conn) {
-     $this->conn = $conn;
-   }
+	function VehiculoDAO(&$conn) {
+		$this->conn = $conn;
+	}
 
-   function save(&$vo) {
-     if ($vo->id == 0) {
-       $this->insert($vo);
-     } else {
-       $this->update($vo);
-     }
-   }
-
-
-   function get($id) {
-     #execute select statement
-     #create new vo and call getFromResult
-     #return vo
-
-     $list = $this->filteredList(" id = '$id' ", "");
-     return  $list[0];
-
-   }
-
-   function delete(&$vo) {
- 
-     $query = "DELETE FROM  $this->TABLE_NAME WHERE id = $vo->id "; 
-   	 $result = mysql_query($query, $this->conn) or db__showError();
-   }
-
-   function filteredList ($filter, $orderby) {
-        $list =  array () ;
-        $query = "SELECT * FROM  $this->TABLE_NAME ";
-        if($filter != "")
-        {
-            $query .= " WHERE $filter ";
-        }
-        if($orderby != "")
-        {
-            $query .= " ORDER BY  $orderby ";
-        }
-       
-        $result = mysql_query($query,$this->conn) or db__showError();
-
-        if ($myrow = mysql_fetch_array($result)) {
-            do {
-                $element = new Vehiculo ();
-                $this->getFromResult($element, $myrow);
-                array_push($list,$element);
-            } while ($myrow = mysql_fetch_array($result));
-        }
-        mysql_free_result($result);
-        return $list;   
-   }
+	function save(&$vo) {
+		if ($vo->id == 0) {
+			$this->insert($vo);
+		} else {
+			$this->update($vo);
+		}
+	}
 
 
-   #-- private functions
+	function get($id) {
+		#execute select statement
+		#create new vo and call getFromResult
+		#return vo
+
+		$list = $this->filteredList(" id = '$id' ", "");
+		return  $list[0];
+
+	}
+
+	function delete(&$vo) {
+
+		$query = "DELETE FROM  $this->TABLE_NAME WHERE id = $vo->id ";
+		$result = mysql_query($query, $this->conn) or db__showError();
+	}
+
+	function filteredList ($filter, $orderby) {
+		$list =  array () ;
+		$query = "SELECT * FROM  $this->TABLE_NAME ";
+		if($filter != "")
+		{
+			$query .= " WHERE $filter ";
+		}
+		if($orderby != "")
+		{
+			$query .= " ORDER BY  $orderby ";
+		}
+			
+		$result = mysql_query($query,$this->conn) or db__showError();
+
+		if ($myrow = mysql_fetch_array($result)) {
+			do {
+				$element = new Vehiculo ();
+				$this->getFromResult($element, $myrow);
+				array_push($list,$element);
+			} while ($myrow = mysql_fetch_array($result));
+		}
+		mysql_free_result($result);
+		return $list;
+	}
+
+
+	#-- private functions
 
 
 
-   function getFromResult(&$vo, $result) {
-     $vo->dump($result);
-     $vo->cliente = $result['idcliente'];
-   }
+	function getFromResult(&$vo, $result) {
+		$vo->dump($result);
+		$vo->cliente = $result['idcliente'];
+	}
 
-   function update(&$vo) {
-   	#execute update statement here
-   	$fieldsStr = "";
-   	$counter = 0;
-   	foreach($vo as $key => $value) {
-   		
-   		if ($key == "cliente") {
-   			$key = "idcliente";
-   		}
-   		
-   		if ($counter > 0) {
-   			$fieldsStr .= ", ";
+	function update(&$vo) {
+		#execute update statement here
+		$fieldsStr = "";
+		$counter = 0;
+		foreach($vo as $key => $value) {
 
-   		}
-   		$fieldsStr .= "$key = '$value'";
-   		$counter++;
-   	}
+			if ($key == "cliente") {
+				$key = "idcliente";
+			}
 
-   	$query = "UPDATE $this->TABLE_NAME SET $fieldsStr WHERE id = '$vo->id' ";
-   	$result = mysql_query($query, $this->conn) or db__showError();
-   }
+			if ($counter > 0) {
+				$fieldsStr .= ", ";
 
-   function insert(&$vo) {
-   	#generate id (from Oracle sequence or automatically)
-   	#insert record into db
-   	#set id on vo
-   	$fieldsStr = "";
-   	$valuesStr = "";
-   	 
-   	$counter = 0;
-   	foreach($vo as $key => $value) {
-   		
-   		if ($key == "cliente") {
-   			$key = "idcliente";
-   		}
-   		
-   		if ( $value != "" ) {
+			}
+			$fieldsStr .= "$key = '$value'";
+			$counter++;
+		}
 
-   			if ($counter > 0) {
-   				$fieldsStr .= ", ";
-   				$valuesStr .= ", ";
-   			}
-   			 
-   			$fieldsStr .= "$key";
-   			$valuesStr .= "'$value'";
-   			$counter++;
-   		}
-   	}
+		$query = "UPDATE $this->TABLE_NAME SET $fieldsStr WHERE id = '$vo->id' ";
+		$result = mysql_query($query, $this->conn) or db__showError();
+	}
 
-   	$query = "INSERT INTO $this->TABLE_NAME ($fieldsStr) VALUES ($valuesStr) ";
-   	$result = mysql_query($query, $this->conn) or db__showError();
-   }
- }
+	function insert(&$vo) {
+		#generate id (from Oracle sequence or automatically)
+		#insert record into db
+		#set id on vo
+		$fieldsStr = "";
+		$valuesStr = "";
+			
+		$counter = 0;
+		foreach($vo as $key => $value) {
 
+			if ($key == "cliente") {
+				$key = "idcliente";
+			}
+
+			if ( $value != "" ) {
+
+				if ($counter > 0) {
+					$fieldsStr .= ", ";
+					$valuesStr .= ", ";
+				}
+					
+				$fieldsStr .= "$key";
+				$valuesStr .= "'$value'";
+				$counter++;
+			}
+		}
+
+		$query = "INSERT INTO $this->TABLE_NAME ($fieldsStr) VALUES ($valuesStr) ";
+		$result = mysql_query($query, $this->conn) or db__showError();
+	}
+
+	/**
+	 * Searches a list of vehicles fitting the criteria based on client an
+	 * vehicle objects.
+	 *
+	 * @param $vo
+	 * @param $client
+	 */
+	function search($vo, $client) {
+		$where = "";
+
+		$vo_prefix = "vehiculos";
+		$cliente_prefix = "clientes";
+
+		foreach($vo as $key => $value) {
+			if ( $value == "" ) {
+				continue;
+			}
+
+			if (!empty($where)) {
+				$where .= " or ";
+			}
+			$where .= "$vo_prefix.$key like '%$value%'";
+		}
+
+		// Client
+		foreach($client as $key => $value) {
+			if ( $value == "" ) {
+				continue;
+			}
+
+			if (!empty($where)) {
+				$where .= " or ";
+			}
+			$where .= "$cliente_prefix.$key like '%$value%'";
+		}
+			
+
+		// Defining final query
+		$list =  array () ;
+		$query = "SELECT distinct vehiculos.* FROM  $this->TABLE_NAME, clientes";
+
+		if ($where != "") {
+			//Joins
+			$where .= " and vehiculos.idcliente=clientes.id";
+			$query .= " WHERE $where";
+		}
+			
+			
+		echo $query;
+		$result = mysql_query($query,$this->conn) or db__showError();
+
+		if ($myrow = mysql_fetch_array($result)) {
+			do {
+				$element = new Vehiculo();
+				$this->getFromResult($element, $myrow);
+				array_push($list,$element);
+			} while ($myrow = mysql_fetch_array($result));
+		}
+
+		mysql_free_result($result);
+		return $list;
+	}
+}
 ?>
